fd4425105c3393ad1582e6960623aa95899057c6
Kimheejune <qwqw0102@hanmail.net>
2014-12-04 15:47:20 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -1,61 +1,28 @@
-var bg;
-var checkDraw = [];
-var drawCount = [];
+var rSlider, gSlider, bSlider;
+
 function setup() {
+  // create canvas
+  createCanvas(710, 400);
+  textSize(15)
+  noStroke();
 
-  createCanvas(1000, 1000);
-  background(32, 42, 55);
-  bg = loadImage("1.png");
-  for(var i = 0; i<4 ;i++){
-    checkDraw[i] = true;
-    drawCount[i] = 0;
-  }   
-
-}
-function draw()
-{
- background(32, 42, 55);
- 
-
- fill(255);
- if(checkDraw[0]){
-    image(bg,0,0);
-  }else{
-    drawCount[0]++;
-    if(drawCount[0] >50){
-      drawCount[0]=0;
-      checkDraw[0]=true;
-    }
-  }
-  if(checkDraw[1]){
-   image(bg,500,0);
-  }else{
-    drawCount[1]++;
-    if(drawCount[1] >50){
-      drawCount[1]=0;
-      checkDraw[1]=true;
-    }
-  }
-  if(checkDraw[2]){
-   image(bg,0,500);
-  }
-  if(checkDraw[3]){
-   image(bg,500,500);
-  }
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
 }
 
-function mousePressed(){
-  if(mouseX >0 && mouseX <500 && mouseY >0 && mouseY <500){
-    checkDraw[0]=false;
-  }
-
-  if(mouseX >500 && mouseX <1000 && mouseY >0 && mouseY <500){
-    checkDraw[1]=false;
-  }
-
+function draw() {
+  var r = rSlider.value();
+  var g = gSlider.value();
+  var b = bSlider.value();
+  background(r, g, b);
+  text("red", 165, 35);
+  text("green", 165, 65);
+  text("blue", 165, 95);
 }
-
-
-
 
 
