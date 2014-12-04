565f9102fe91a4ef02d8682432370413f4ab0bea
Kimheejune <qwqw0102@hanmail.net>
2014-12-04 14:35:40 +0900

    4144

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 var bg;
-var countCat;
-var bigSizeW=462;
-
 
 function setup() {
 
     createCanvas(1000, 1000);
-    noFill(98, 71, 73);
 
     bg = loadImage("momo.png");
     
- 
+
+}
+function draw()
+{
+ background(bg);
+
+}
